6e6b34a8062c9868d4b70cac47dd90a15a5a931e
Narendran <175111990+Twenty625@users.noreply.github.com>
2025-01-18 05:32:55 +0530

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const arr = new Array(word.length);
+    let rev = '';
+    for (let i=0; i<word.length; i++) {
+        arr[i] = word[word.length - i - 1];
+        rev = rev + arr[i];
+    }
+    return rev;
 };
 
 // Do not edit below this line
